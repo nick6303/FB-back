4b28890c934eee8eb356082b28f24590a220cb18
nick <nick@example.com>
2022-05-01 15:06:32 +0800

    udpate

--- models/user.js
+++ models/user.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose')
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, '請輸入您的名字'],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, '請輸入您的名字'],
+    },
+    email: {
+      type: String,
+      required: [true, '請輸入您的 Email'],
+      unique: true,
+      lowercase: true,
+      select: false,
+    },
+    photo: {
+      type: String,
+      default: '',
+    },
   },
-  email: {
-    type: String,
-    required: [true, '請輸入您的 Email'],
-    unique: true,
-    lowercase: true,
-    select: false,
-  },
-  photo: String,
-})
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+)
 
 const User = mongoose.model('user', userSchema)
 
